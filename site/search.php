fb3a4f77bce44c34f032be64ebc06dd7a9ac15a6
pthomas931 <pyromaniac931@yahoo.com>
2011-02-10 21:43:47 -0500

    Here is the front end.

---
+++ site/search.php
@@ -0,0 +1,2 @@
+<button id="buttonsearch" type="buttonsearch" onclick="">Search!</button>
+<input id="search" type="search" value="search" />
---
+++ site/strategies.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Zombie Apocalypse Survival</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="wrapper">
+<!-- start header -->
+<?php
+	include 'header.php';
+?>
+<!-- end header -->
+<!-- start page -->
+<div id="page">
+	<!-- start content -->
+	<div id="content">
+		<div class="post">
+			<h1 class="title">Phillip's Strategy</h1>
+			<p class="byline"><small>Posted on February 10th, 2011 by <a href="#">admin</a> | <a href="#">Edit</a></small></p>
+			<div class="strat">
+				<p>I'm going to pack up and head North. Zombies have no body heat and will freeze solid.</p>
+				<p>I will live off the land ice fishing, hunting, and gathering</p>
+			</div>
+			<p class="meta"><a href="#" class="more">Read More</a> &nbsp;&nbsp;&nbsp; <a href="#" class="comments">Comments (0)</a></p>
+		</div>
+		<div class="post">
+			<h1 class="title">Bob's Strategy</h1>
+			<p class="byline"><small>Posted on February 10th, 2011 by <a href="#">admin</a> | <a href="#">Edit</a></small></p>
+			<div class="strat">
+				<p>Well, I'm going to die anyways. I'm just going to put on kevlar and a helmet. </p><p>Good luck survivors!</p>
+			</div>
+			<p class="meta"><a href="#" class="more">Read More</a> &nbsp;&nbsp;&nbsp; <a href="#" class="comments">Comments (0)</a></p>
+		</div>
+	</div>
+	<!-- end content -->
+	<!-- start sidebar -->
+	<?php
+		include "sidebar.php";
+	?>
+	<!-- end sidebar -->
+</div>
+</div>
+<!-- end page -->
+<div id="footer">
+	<p>&copy;2007 All Rights Reserved &nbsp;&bull;&nbsp; Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a> &nbsp;&bull;&nbsp; Icons by <a href="http://www.famfamfam.com/">FAMFAMFAM</a>.</p>
+</div>
+</body>
+</html>
