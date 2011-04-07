47fd26eca057127f56a069f14d64d7356d5051e8
Matt <mmiller7@mail.umw.edu>
2011-04-06 22:09:19 -0400

    added comments

--- site/anti_xss.php
+++ site/anti_xss.php
@@ -8,9 +8,14 @@ if(!$anti_xss_function_exists)
 	{
 		//strip_tags syntax the second parameter lists all ALLOWED html tags
 		$raw = strip_tags($raw,'<b>,<i>,<u>,<font>,<li>,<ul>,<ol>,<p>');
+
+		//parse newlines as <br> -- don't allow <br> because we only
+		//want it to be allowed on forms that support multi-line entry
 		$raw = nl2br($raw);
 		$raw = str_replace("\r",'',$raw);
 		$raw = str_replace("\n",'',$raw);
+
+		//return the result
 		return $raw;
 	}
 }
